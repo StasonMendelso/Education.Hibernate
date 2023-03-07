16941438067b56bc8b37ea98c488dd36b783849f
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-03-07 09:14:32 +0200

    notes, loadings: notes about lazy & eager loading.
    Making notes about lazy & eager loading in Hibernate.

--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -3,11 +3,11 @@ package org.example.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
@@ -37,9 +37,6 @@ public class Person {
     @Cascade(CascadeType.SAVE_UPDATE)
     private List<Item> itemList;
 
-    @OneToOne(mappedBy = "person")
-    @Cascade(CascadeType.SAVE_UPDATE)
-    private Passport passport;
     public Person() {
     }
 
@@ -48,14 +45,6 @@ public class Person {
         this.age = age;
     }
 
-    public Passport getPassport() {
-        return passport;
-    }
-
-    public void setPassport(Passport passport) {
-        this.passport = passport;
-        passport.setPerson(this);
-    }
 
     public int getId() {
         return id;
